16f1977868cc9c95cd99bffa85eed3846b8c9c3c
kudaakwashe <kudaakwashe@gmail.com>
2021-03-06 18:47:29 +0200

    Update settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b52r7u(m#05dy0$6k$&w=zp^%2*2q@n3tmi7pdu&)!nn#0asrw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
